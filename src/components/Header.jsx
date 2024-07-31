d10bec5289f2916b96292c9866c8ad315e9df161
DarkSharkk <telio2016@yandex.ru>
2024-07-31 17:46:57 +0300

    git initialization

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://www.svgrepo.com/show/8321/networking.svg" />
+    </header>
+  );
+};
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div>Profile</div>
+      <div>Messages</div>
+      <div>Settings</div>
+    </nav>
+  );
+};
