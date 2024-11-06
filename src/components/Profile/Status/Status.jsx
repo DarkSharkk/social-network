0bcf2248fe37879db710da279fb6fa5d3292ea2d
Shamsudin Gamidov <telio2016@yandex.ru>
2024-11-06 17:54:18 +0300

    profile status update fix

--- src/components/Profile/Status/Status.jsx
+++ src/components/Profile/Status/Status.jsx
@@ -18,6 +18,12 @@ export class Status extends React.Component {
             this.props.updateStatus(this.state.status);
         }
     }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({ status: this.props.status });
+        }
+    }
     
     render() {
         return (
